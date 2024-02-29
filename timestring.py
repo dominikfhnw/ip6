005ec51146d01b7fdb30c2ad87bac59cc50e28d6
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-02-29 14:00:46 +0100

    add function to return iso8601 datetime WITH timezone offset

--- timestring.py
+++ timestring.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+import datetime
 
 def timestring():
-    datetime.now().strftime("%Y%m%d-%H%M%S")
+    return datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def iso8601():
+    return datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat(timespec="seconds")
