5e10f0c27b8ca1719cb4480f27cb67ee2dde9200
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-02-29 14:00:10 +0100

    add documentation

--- timey.py
+++ timey.py
@@ -1,4 +1,9 @@
 from time import time
+# purpose of this module:
+# - load it as first module, and note start time as soon as possible,
+#   without linter complaining about code between imports
+# - provide simple helper function to get time delta from start
+# - provide time()
 
 t0 = time()
 
