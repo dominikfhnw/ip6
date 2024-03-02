018f24a4f8e308276d41cab3e6917db7b18b8867
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-03-03 00:08:29 +0100

    rework segments as a module for the existing framework

--- segments.py
+++ segments.py
@@ -5,10 +5,77 @@ import numpy as np
 
 log, dbg, logger = log.auto(__name__)
 
-FACTOR = 3
+#FACTOR = 3
 
-def process(img):
-    log("hello world")
+def process(img, height):
+    # TODO: ugly
+    global FACTOR
+    FACTOR=int(height/100)
+
+    img = img - np.min(img)
+    img = img * (256/np.max(img))
+    img = cv.cvtColor(img.astype("uint8"), cv.COLOR_GRAY2BGR)
+
+    x1, y1 = 0,0
+    start=0
+    l = np.empty((6,7))
+
+    xoffset, xmax, xstep = 12, 250, 46
+    for i in range(xoffset, xmax, xstep):
+        x = letter(img, i,x1,y1,str(start))
+        l[start] = x
+        start += 1
+    dbg("NUMBER: "+str(l))
+    min = np.min(l)
+    #l2 = np.subtract(l, min)
+    l2 = l - min
+    max = np.max(l2)
+    l2 = l2 / max
+    l2 = 1 - l2
+    l2 = np.round(l2)
+    dbg("NUMBER2: "+str(l2))
+
+    # alternative font
+    a = 0
+    digits = np.array([
+        [1, 1, 1, 1, 1, 1, 0], # 0
+        [0, 1, 1, 0, 0, 0, 0], # 1
+        [1, 1, 0, 1, 1, 0, 1], # 2
+        [1, 1, 1, 1, 0, 0, 1], # 3
+        [0, 1, 1, 0, 0, 1, 1], # 4
+        [1, 0, 1, 1, 0, 1, 1], # 5
+        [a, 0, 1, 1, 1, 1, 1], # 6
+        [1, 1, 1, 0, 0, 0, 0], # 7
+        [1, 1, 1, 1, 1, 1, 1], # 8
+        [1, 1, 1, a, 0, 1, 1], # 9
+        #[0, 0, 0, 0, 0, 0, 1], # -
+        #[0, 0, 0, 0, 0, 0, 0], # (blank)
+    ])
+    i=0
+    out=""
+    for num in l2:
+        result=-1
+        dbg(f"NUM {i}: {l2[i]}")
+        j=0
+        for ref in digits:
+            if np.array_equal(digits[j], l2[i]):
+                dbg(f"MATCH {j}")
+                result=j
+            j += 1
+        if result >= 0:
+            n = str(result)
+        else:
+            n = "#"
+        out += n
+        cv.putText(img, n,
+                   (FACTOR*(xoffset+i*xstep),FACTOR*95),
+                   fontFace=cv.FONT_HERSHEY_DUPLEX,
+                   fontScale=1,
+                   color=(255, 0, 0)
+        )
+        i += 1
+    cv.imshow("ocr", img)
+    log(f"Number: {out}")
 
 def rec(img,x,y,w,h,color=(0,0,255), name=None):
     x2=x+w
@@ -19,8 +86,11 @@ def rec(img,x,y,w,h,color=(0,0,255), name=None):
     x2 *= FACTOR
     y2 *= FACTOR
 
-    #log("SHAPE "+str(img.shape))
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    #log("SHAPE "+str(img.ndim))
+    if img.ndim == 3:
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    else:
+        gray = img.copy()
     i2 = gray[y:y2, x:x2]
     #cv.imshow("seg", i2)
     #cv.waitKey(0)
@@ -65,65 +135,11 @@ def seg():
     cv.imshow("seg", input)
     #segments.process(input, None)
     #
-    x1, y1 = 0,0
+
     while True:
         img = input.copy()
-        start=0
-        letters = dict()
-        l = np.empty((6,7))
-
-        for i in range(12, 250, 46):
-            x = letter(img, i,x1,y1,str(start))
-            #print("L "+str(x))
-            #print("sha "+str(x.shape))
-            letters[start] = x
-            #l.append(x)
-            l[start] = x
-            start += 1
-        dbg("NUMBER: "+str(l))
-        min = np.min(l)
-        #l2 = np.subtract(l, min)
-        l2 = l - min
-        max = np.max(l2)
-        l2 = l2 / max
-        l2 = 1 - l2
-        l2 = np.round(l2)
-        dbg("NUMBER2: "+str(l2))
-
-
-        # alternative font
-        a = 0
-        digits = np.array([
-            [1, 1, 1, 1, 1, 1, 0], # 0
-            [0, 1, 1, 0, 0, 0, 0], # 1
-            [1, 1, 0, 1, 1, 0, 1], # 2
-            [1, 1, 1, 1, 0, 0, 1], # 3
-            [0, 1, 1, 0, 0, 1, 1], # 4
-            [1, 0, 1, 1, 0, 1, 1], # 5
-            [a, 0, 1, 1, 1, 1, 1], # 6
-            [1, 1, 1, 0, 0, 0, 0], # 7
-            [1, 1, 1, 1, 1, 1, 1], # 8
-            [1, 1, 1, a, 0, 1, 1], # 9
-            [0, 0, 0, 0, 0, 0, 1], # -
-            [0, 0, 0, 0, 0, 0, 0], # (blank)
-        ])
-        i=0
-        out=""
-        for num in l2:
-            result=-1
-            dbg(f"NUM {i}: {l2[i]}")
-            j=0
-            for ref in digits:
-                if np.array_equal(digits[j], l2[i]):
-                    dbg(f"MATCH {j}")
-                    result=j
-                j += 1
-            if result >= 0:
-                out += str(result)
-            else:
-                out += "#"
-            i += 1
-        log(f"Number: {out}")
+        log(str(img.shape[0]))
+        process(img, img.shape[0])
         #dbg(f'MIN {min} MAX {max}')
         cv.imshow("seg", img)
         cv.waitKey(0)
